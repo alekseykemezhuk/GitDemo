585359ea1ff811cfd225321d5207ca8c3620da68
Aleksey Kemezhuk <aleksey.kemezhuk@gmail.com>
2025-07-08 11:23:22 +0200

    Project added

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Aleksey Kemezhuk on 06/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
---
+++ GitDemoTests/GitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoTests.swift
+//  GitDemoTests
+//
+//  Created by Aleksey Kemezhuk on 06/07/2025.
+//
+
+import Testing
+@testable import GitDemo
+
+struct GitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
