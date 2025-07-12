15222d9e200d00af254529c47f09a528458a155d
Aleksey Kemezhuk <aleksey.kemezhuk@gmail.com>
2025-07-12 11:00:08 +0200

    Third button added

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     // MARK: - Properties
     private var button = UIButton(configuration: .gray())
     private var secondButton = UIButton(configuration: .gray())
+    private var thirdButton = UIButton(configuration: .gray())
     
     // MARK: - VC Lifecycle
     override func viewDidLoad() {
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
     private func setupUI() {
         view.addSubview(button)
         view.addSubview(secondButton)
+        view.addSubview(thirdButton)
+        
         button.setTitle("I'm good, yeah I'm feeling alright!", for: .normal)
         button.addAction(UIAction(handler: { _ in
             print("action")
@@ -32,12 +35,21 @@ class ViewController: UIViewController {
         button.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+        
         secondButton.setTitle("Thank You very much!", for: .normal)
         secondButton.addAction(UIAction(handler: { _ in
             print("action")
         }) , for: .touchUpInside)
         secondButton.snp.makeConstraints { make in
             make.center.equalTo(button).offset(50)
+        }
+        
+        thirdButton.setTitle("I'm good, yeah I'm feeling alright!", for: .normal)
+        thirdButton.addAction(UIAction(handler: { _ in
+            print("action")
+        }) , for: .touchUpInside)
+        thirdButton.snp.makeConstraints { make in
+            make.center.equalTo(secondButton).offset(50)
         }
     }
 
