43327d4599bd6331543c8de412cfe87dd52868c7
Aleksey Kemezhuk <aleksey.kemezhuk@gmail.com>
2025-07-12 08:11:22 +0200

    Second button added

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     private var button = UIButton(configuration: .gray())
+    private var secondButton = UIButton(configuration: .gray())
     
     // MARK: - VC Lifecycle
     override func viewDidLoad() {
@@ -23,12 +24,20 @@ class ViewController: UIViewController {
     // MARK: - Flow Methods
     private func setupUI() {
         view.addSubview(button)
+        view.addSubview(secondButton)
         button.setTitle("I'm good!", for: .normal)
         button.addAction(UIAction(handler: { _ in
             print("action")
         }) , for: .touchUpInside)
         button.snp.makeConstraints { make in
             make.center.equalToSuperview()
+        }
+        secondButton.setTitle("Thank You!", for: .normal)
+        secondButton.addAction(UIAction(handler: { _ in
+            print("action")
+        }) , for: .touchUpInside)
+        secondButton.snp.makeConstraints { make in
+            make.center.equalTo(button).offset(50)
         }
     }
 
